cedf98912ce2c5cf87628b49c8a72bf1d1dea552
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-07-14 12:50:10 +0530

    Update Site.js

--- src/components/Stats/Site.js
+++ src/components/Stats/Site.js
@@ -5,7 +5,7 @@ import initialData from "../../data/stats/site";
 const Stats = () => {
   const [data, setResponseData] = useState(initialData);
   // TODO think about persisting this somewhere
-  
+
   const fetchData = useCallback(async () => {
     // request must be authenticated if private
     const res = await fetch(
@@ -13,16 +13,22 @@ const Stats = () => {
     );
     const resData = await res.json();
 
-    const linesOfJs = await fetch("https://ghloc.vercel.app/api/gnpaone/naveen-personal-website/badge?filter=.js");
-    const linesOfJsData = await linesOfJs.json();
-    
+    const linesOfJs = await fetch(
+      "https://api.allorigins.win/get?url=https://ghloc.vercel.app/api/gnpaone/naveen-personal-website/badge?filter=.js$",
+    );
+    const linesOfJsWrapper = await linesOfJs.json();
+    const linesOfJsData = JSON.parse(linesOfJsWrapper.contents);
+
     setResponseData(
       initialData.map((field) => ({
         ...field,
         // update value if value was returned by call to github
-        value: field.label === "Lines of Javascript powering this website" 
-          ? linesOfJsData.message 
-          : (Object.keys(resData).includes(field.key) ? resData[field.key] : field.value),
+        value:
+          field.label === "Lines of Javascript powering this website"
+            ? linesOfJsData.message
+            : Object.keys(resData).includes(field.key)
+              ? resData[field.key]
+              : field.value,
       })),
     );
   }, []);
