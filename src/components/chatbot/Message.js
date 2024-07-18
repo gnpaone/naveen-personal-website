1aaab63e52af94bcd8bb228750201c153a29518b
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-07-18 19:35:35 +0530

    Update Message.js

--- src/components/chatbot/Message.js
+++ src/components/chatbot/Message.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+var he = require("he");
+
 const Message = (props) => {	
   return(<div className={props.speaks === "me" ? "right-align" : ""}>
       <div className={`chatbubble ${props.speaks === "bot" ? "bot" : "me"}`}>
-        <span>{props.text}</span>
+        {props.speaks === "bot" ? (
+          <span dangerouslySetInnerHTML={{ __html: he.decode(props.text[0]) }} />
+        ) : (
+          <span>{props.text}</span>
+        )}
       </div>
   </div>
 )};
